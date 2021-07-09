1a86fd8607be2a56e384652357e2f95b13b9af45
FatemaNeemuch <fatemaneemuchwala@gmail.com>
2021-07-09 17:29:46 -0500

    endless scrolling, pink, compose layout

--- app/src/main/java/com/codepath/parstagram/fragments/PostsFragment.java
+++ app/src/main/java/com/codepath/parstagram/fragments/PostsFragment.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.codepath.parstagram.EndlessRecyclerViewScrollListener;
 import com.codepath.parstagram.Post;
 import com.codepath.parstagram.PostsAdapter;
 import com.codepath.parstagram.R;
@@ -39,6 +40,7 @@ public class PostsFragment extends Fragment {
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
     protected SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,7 +61,20 @@ public class PostsFragment extends Fragment {
         //set adapter
         rvPosts.setAdapter(adapter);
         //set layout manager
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(linearLayoutManager);
+
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(page);
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvPosts.addOnScrollListener(scrollListener);
 
         // Lookup the swipe container view
         swipeContainer = view.findViewById(R.id.swipeContainer);
@@ -71,6 +86,7 @@ public class PostsFragment extends Fragment {
                 // Make sure you call swipeContainer.setRefreshing(false)
                 // once the network request has completed successfully.
                 fetchTimelineAsync(PAGE);
+                scrollListener.resetState();
             }
         });
         // Configure the refreshing colors
@@ -81,6 +97,40 @@ public class PostsFragment extends Fragment {
 
         //get posts
         queryPosts();
+    }
+
+    // Append the next page of data into the adapter
+    // This method probably sends out a network request and appends new data items to your adapter.
+    public void loadNextDataFromApi(int offset) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include data referred by user key
+        query.include(Post.KEY_USER);
+        //skip over the posts already pulled
+        query.setSkip(allPosts.size());
+        // limit query to latest 20 items
+        query.setLimit(QUERY_LIMIT);
+        // order posts by creation date (newest first)
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        // start an asynchronous call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, getString(R.string.query_post_log), e);
+                    return;
+                }
+                //save received posts to list
+                allPosts.addAll(posts);
+                //notify adapter of new data
+                adapter.notifyItemRangeChanged(allPosts.size() - posts.size(), posts.size());
+            }
+        });
     }
 
     public void fetchTimelineAsync(int page) {
